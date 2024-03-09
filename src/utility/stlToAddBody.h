5490814fe33ae5be68941eb28d4a6034d49893a5
programmerToff <121182260+programmerToff@users.noreply.github.com>
2024-03-09 16:44:23 +0100

    Add files via upload

--- src/utility/stlToAddBody.h
+++ src/utility/stlToAddBody.h
@@ -5,14 +5,16 @@ inline void stlToAddBody()
 {
 	bodies bodySTL;
 	bodySTL.readSTL();
-	std::cout << "Body body(0);\n";
-	for (int i = 0; i < bodySTL.BodyList.first[0].verts.size() / 6; i += 6)
+	bodySTL.optimizeVertexCount(0);
+	std::ofstream output(saveFileDialog());
+	output << "Body body(0);\n";
+	for (int i = 0; i < bodySTL.BodyList.first[0].verts.size(); i += 6)
 	{
-		std::cout << "addVertex( body.verts," << bodySTL.BodyList.first[0].verts[i] << "," << bodySTL.BodyList.first[0].verts[i + 1] << "," << bodySTL.BodyList.first[0].verts[i + 2] << " );\n";
+		output << "addVertex( body.verts," << bodySTL.BodyList.first[0].verts[i] << "," << bodySTL.BodyList.first[0].verts[i + 1] << "," << bodySTL.BodyList.first[0].verts[i + 2] << " );\n";
 	}
-	for (int i = 0; i < bodySTL.BodyList.first[0].inds.size() / 3; i += 3)
+	for (int i = 0; i < bodySTL.BodyList.first[0].inds.size(); i += 3)
 	{
-		std::cout << "addTriangle(body.inds," << bodySTL.BodyList.first[0].inds[i] << "," << bodySTL.BodyList.first[0].inds[i + 1] << "," << bodySTL.BodyList.first[0].inds[i + 2] << " );\n";
+		output << "addTriangle(body.inds," << bodySTL.BodyList.first[0].inds[i] << "," << bodySTL.BodyList.first[0].inds[i + 1] << "," << bodySTL.BodyList.first[0].inds[i + 2] << " );\n";
 	}
-	std::cout << "BodyList.first.push_back(body);\nBodyList.second.push_back(\"Body\");";
+	output << "return body;";
 }
